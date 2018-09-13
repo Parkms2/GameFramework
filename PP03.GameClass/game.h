d51b1fe7f3f11ad86c2b59fc40d86c55fee85eb1
문수 박 <20171153@vision.hoseo.edu>
2018-09-13 17:08:46 +0900

    9월 13일 PP03.GameClass

---
+++ PP03.GameClass/game.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "SDL.h"
+#include <iostream>
+
+
+class Game {
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEventes();
+	void clean();
+	bool runnung() { return m_bRunning; }
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+};
