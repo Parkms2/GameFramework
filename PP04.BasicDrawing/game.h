46360138e98800e95ab488c1568b4ff66da953df
문수 박 <20171153@vision.hoseo.edu>
2018-09-13 17:48:50 +0900

    흠..

---
+++ PP04.BasicDrawing/game.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include"SDL.h"
+#include <iostream>
+
+class Game {
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEventes();
+	void clean();
+	bool runnung() { return m_bRunning; }
+	bool m_bRunning;
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	
+
+
+	SDL_Texture* m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
+};
---
+++ PP04.BasicDrawing/main.cpp
@@ -0,0 +1,18 @@
+#include"game.h"
+
+Game* g_game = 0;
+
+int main(int argc, char* argv[]) {
+	g_game = new Game();
+	g_game->init("Chapter 1", 100, 100, 640, 480, false);
+	g_game->m_bRunning = true;
+	while (g_game->runnung()) {
+		g_game->handleEventes();
+		g_game->update();
+		g_game->render();
+	}
+
+	SDL_Delay(1000);
+	g_game->clean();
+	return 0;
+}
