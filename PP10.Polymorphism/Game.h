44def86fb562e4a4e8208ec94b0aa84e4c5d7fbb
문수 박 <20171153@vision.hoseo.edu>
2018-10-18 18:05:47 +0900

    PP10.Polymorphism 10월 18일

---
+++ PP10.Polymorphism/Game.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include"SDL.h"
+#include <iostream>
+#include<vector>
+#include"TextureManager.h"
+#include"GameObject.h"
+#include"Player.h"
+#include"Enemy.h"
+
+class Game {
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEventes();
+	void clean();
+	bool running() { return m_bRunning; }
+
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+
+	int m_currentFrame;
+
+	std::vector<GameObject*> m_gameObjects;
+	GameObject* m_go;
+	GameObject* m_player;
+	GameObject* m_enemy;
+
+};
