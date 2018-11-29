d892173aa4ff320ed1d2c93439cfce64278e4a6a
박문수 <20171153@vision.hoseo.edu>
2018-11-29 17:07:46 +0900

    11/29 PP18.PauseState

---
+++ PP18.PauseState/Game.cpp
@@ -0,0 +1,42 @@
+#include "game.h"
+#include<SDL_image.h>
+
+Game* Game::s_pInstance = 0;
+
+bool Game::init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+		if (m_pWindow != 0) {
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+			m_bRunning = true;
+			SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
+
+			m_pGameStateMachine = new GameStateMachine();
+			m_pGameStateMachine->changeState(MenuState::Instance());
+
+		}
+		else return false;
+	}
+	return true;
+}
+void Game::render() {
+	SDL_RenderClear(m_pRenderer);
+	m_pGameStateMachine->render();
+	SDL_RenderPresent(m_pRenderer);
+}
+void Game::update() {
+	m_pGameStateMachine->update();
+}
+void Game::handleEvents() {
+	TheInputHandler::Instance()->update();
+}
+void Game::clean() {
+	std::cout << "cleaning game\n";
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+	//TheInputHandler::Instance()->clean();
+}
+void Game::quit() {
+	m_bRunning = false;
+}
---
+++ PP18.PauseState/PlayState.cpp
@@ -0,0 +1,43 @@
+﻿#include "PlayState.h"
+#include"Player.h"
+#include"Enemy.h"
+#include"PauseState.h"
+const std::string PlayState::s_playID = "PLAY";
+PlayState* PlayState::s_pInstance = 0;
+void PlayState::update()
+{
+	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_ESCAPE)) {
+		TheGame::Instance()->getStateMachine()->changeState(PauseState::Instance());
+	}
+	for (int i = 0; i < m_gameObjects.size(); i++) {
+		m_gameObjects[i]->update();
+	}
+}
+void PlayState::render()
+{
+	for (int i = 0; i < m_gameObjects.size(); i++) {
+		m_gameObjects[i]->draw();
+	}
+}
+bool PlayState::onEnter()
+{
+	if (!TheTextureManager::Instance()->load( "assets/helicopter.png", "helicopter", TheGame::Instance()->getRenderer())) {
+		return false;
+	}
+	GameObject* player = new Player(new LoaderParams(100, 100, 128, 55, "helicopter"));
+	m_gameObjects.push_back(player);
+	std::cout << "entering PlayState\n";
+	return true;
+
+}
+
+bool PlayState::onExit()
+{
+	for (int i = 0; i < m_gameObjects.size(); i++) {
+		m_gameObjects[i]->clean();
+	}
+	m_gameObjects.clear();
+	TheTextureManager::Instance()->clearFromTextureMap("helicopter");
+	std::cout << "exiting PlayState\n";
+	return true;
+}
