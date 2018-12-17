721f240526fdd70dd8da2e6bfd0a2e776ec18eb6
박문수 <20171153@vision.hoseo.edu>
2018-12-17 21:56:15 +0900

    완성

--- miniGame/Enemy.cpp
+++ miniGame/Enemy.cpp
@@ -1,7 +1,7 @@
 #include"Enemy.h"
 Enemy::Enemy(const LoaderParams* pParams) : SDLGameObject(pParams)
 {
-	die = SDL_GetTicks() + 2000;	//생성후 2초후 죽음
+	die = SDL_GetTicks() + 7000;	//생성후 2초후 죽음
 	setEnemyPosition = rand() % 820 - 100;
 	if (setEnemyPosition <= -45 || setEnemyPosition >= 720) {	//x축이 양수일때 y가 보이지 않는 범위에서 생성
 		m_position.setX(rand() % 400);
